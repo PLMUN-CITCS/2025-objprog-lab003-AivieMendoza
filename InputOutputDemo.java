646ac846e85ba61b7feed23b62c37943bd47bd81
AivieMendoza <mendozafrancesaivie_bsit@plmun.edu.ph>
2025-02-13 21:40:45 +0800

    Submitting OBJPROG Week 02 - Session 01 - Exercise 01

--- InputOutputDemo.java
+++ InputOutputDemo.java
@@ -6,12 +6,10 @@ public class InputOutputDemo {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter an integer: ");
         int userInt = input.nextInt();
-
         System.out.print("Enter a decimal number: ");
         double userDouble = input.nextDouble();
         input.nextLine(); // Consume newline
-
-      
+		
         System.out.print("Enter a word or sentence: ");
         String userString = input.nextLine();
         System.out.printf("Your integer: %d%n", userInt);
